66d78daeaf5214d72db5b0c8a71c2175ef733aa6
Gregg Jenczyk <gjenczyk@outlook.com>
2017-12-11 22:13:05 -0500

    Lets see if this works

---
+++ StarRez/StarRez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarRez
+//
+//  Created by gj3 on 12/11/17.
+//  Copyright © 2017 gj3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
